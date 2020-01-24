bccd720108032b3294c6e92c930626203ccede64
Teerth Patel <teerth1205@gmail.com>
2020-01-24 23:10:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,8 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   
-holiday_hash[season][ho.holiday_name] = supply_array
+holiday_hash[season] = holiday_name 
+season[holiday_name] = supply_array
 holiday_hash
   # code here
   # remember to return the updated hash
