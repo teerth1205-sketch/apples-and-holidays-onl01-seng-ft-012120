76ed0431aa192f2446461212623bb44432de626a
Teerth Patel <teerth1205@gmail.com>
2020-01-24 23:40:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,6 +85,7 @@ def all_supplies_in_holidays(holiday_hash)
      end 
    end 
  end 
+ end
         
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
